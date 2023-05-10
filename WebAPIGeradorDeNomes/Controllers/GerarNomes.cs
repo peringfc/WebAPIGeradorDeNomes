639b992cd872770cda2807bb6a00ec82692604e1
christian.perin <christian.perin@OSA0625213W10-1>
2023-05-09 21:11:36 -0300

    Adicionar arquivos de projeto.

---
+++ WebAPIGeradorDeNomes/Controllers/GerarNomes.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebAPIGeradorDeNomes.Regras;
+
+namespace WebAPIGeradorDeNomes.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GerarNomes : ControllerBase
+    {
+        [HttpGet(Name = "GeradorNomes")]
+        public string GeraNomes()
+        {
+            GeredorDenomes geredor = new GeredorDenomes();
+
+            string Nome = geredor.mulher();
+            Nome = Nome + geredor.sobrenome();
+
+            return Nome;
+        }
+    }
+}
